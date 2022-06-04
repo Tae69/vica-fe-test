86a054c675cdf3a1f1babc36e15c05f1d90bde1d
Hermanto Tang <hermanto@ivs.tv>
2022-06-04 20:16:10 +0700

    Add confirmation dialog for deleting logged in user

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,7 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Button, Typography, Container, Paper, TextField } from '@mui/material';
+import {
+  Box,
+  Button,
+  Typography,
+  Container,
+  Paper,
+  TextField,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions
+} from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import SearchIcon from '@mui/icons-material/Search';
+import { useNavigate } from 'react-router-dom';
 import Page from '../components/Page';
 import TopBar from '../components/TopBar';
 import EnhancedTable from '../components/UserTable';
@@ -12,14 +25,19 @@ import LinkBehavior from '../components/LinkBehavior';
 import { useAppDispatch, useAppSelector } from '../hooks/redux';
 import { openNotification } from '../reducers/notification';
 import ErrorMessage from '../enums/error';
-import { selectCurrentUser } from '../reducers/user';
+import { logout, selectCurrentUser } from '../reducers/user';
 
 function Users() {
   const [rows, setRows] = useState<User[]>([]);
   const [keyword, setKeyword] = useState('');
 
+  const [open, setOpen] = useState(false);
+  const [confirm, setConfirm] = useState(false);
+  const [pendingDeletionId, setPendingDeletionId] = useState<number | null>(null);
+
   const token = useApi();
 
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const user = useAppSelector(selectCurrentUser)!;
 
@@ -31,11 +49,26 @@ function Users() {
 
   const removeUser = async (id: number) => {
     try {
+      const deleteOwnUser = id === user.id;
+      if (deleteOwnUser && !confirm) {
+        setPendingDeletionId(id);
+        setOpen(true);
+        return;
+      }
+
+      setPendingDeletionId(null);
+
       await api.removeUser(token, id);
       const index = rows.findIndex((r) => r.id === id);
       if (index > -1) {
         rows.splice(index, 1);
         setRows([...rows]);
+      }
+
+      if (deleteOwnUser) {
+        dispatch(logout());
+        navigate('/');
+        return;
       }
 
       dispatch(openNotification({ message: 'User deleted successfully.' }));
@@ -45,6 +78,17 @@ function Users() {
       dispatch(openNotification({ message, severity: 'error' }));
     }
   };
+
+  const handleClose = (value: boolean = false) => {
+    setOpen(false);
+    setConfirm(value);
+  };
+
+  useEffect(() => {
+    if (confirm && pendingDeletionId) {
+      removeUser(pendingDeletionId);
+    }
+  }, [confirm]);
 
   return (
     <>
@@ -80,6 +124,27 @@ function Users() {
             <EnhancedTable rows={rows} onRemove={removeUser} canModify={user.role === Role.Admin} />
           </Paper>
         </Container>
+        <Dialog
+          open={open}
+          onClose={() => handleClose()}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            The user you are deleting is the current user you use, are you sure want to delete?
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              When you delete logged in user, you will be logged out when you confirm the deletion.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => handleClose()}>No</Button>
+            <Button onClick={() => handleClose(true)} autoFocus>
+              Yes
+            </Button>
+          </DialogActions>
+        </Dialog>
       </Page>
     </>
   );
